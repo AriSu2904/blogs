e1de68a29b98c1babd7a8428fe43e47a704f5c78
AriSu2904 <arisusanto290401@gmail.com>
2024-06-23 20:46:42 +0700

    login screen

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['montserrat', 'sans-serif'],
+        poppins: ['poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
